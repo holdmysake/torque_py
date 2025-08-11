4cf5d6829b6661f4718521d4685ceae32f66ae29
sahdan17 <sahdan.wira.17@gmail.com>
2025-08-11 14:48:14 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     apps: [
         {
-            name: "torque-py",
+            name: "torque-py", // ganti sesuai nama project
             script: "uvicorn",
-            args: "main:app --host 0.0.0.0 --port 1111",
-            interpreter: "/_workdir_sawir/torque_py/venv/bin/python",
+            args: "main:app --host 0.0.0.0 --port 1111 --reload",
+            interpreter: "python3", // interpreter global
             watch: false,
             env: {
                 PYTHONUNBUFFERED: "1"
